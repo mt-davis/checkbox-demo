f43a7f065c85584108c28b92269ab3458708d84d
M.T. Davis <marquese.davis@gmail.com>
2016-07-15 15:54:20 +0000

    add: added dummy data in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+expertise = [
+    "Astronomy", "Biology", "Chemistry", "English", "Spanish", "Latin", "Math", "Medicine", "Physics", "Swimming", "Programming" ]
+
+expertise.each do |name|
+    Expertise.create(name: name)
+end
+
+professors = [
+    "Emily Ferguson",
+    "Roy Mcdonald",
+    "Shawn Kim",
+    "Jane Simpson",
+    "Laura Howard",
+    "Rebecca Henry",
+    "Cheryl Perkins",
+    "Rachel Bennett",
+    "Wayne Ross",
+    "Jeffrey Perez",
+    "Marilyn Stanley",
+    "Ear Vasquez",
+    "William Cook",
+    "Chris George",
+    "Deborah	Hunt",
+    "Jeremy Mendoza",
+    "Jack Reed",
+    "Tammy Simpson",
+    "Judy Wright",
+    "Jesse Reed"
+]
+
+professors.each do |name|
+  Professor.create(name: name)
+end
